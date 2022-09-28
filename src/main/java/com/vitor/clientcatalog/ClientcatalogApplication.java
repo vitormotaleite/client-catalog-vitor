a2f902c4c524ba5f313cbed496c6fa91f36962d7
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-28 09:52:41 -0300

    criado projeto

---
+++ src/main/java/com/vitor/clientcatalog/ClientcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.clientcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientcatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitor/clientcatalog/ClientcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.clientcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
