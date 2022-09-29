1b75bf207c58b51c14f18af25323d93108b371a4
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-29 11:24:49 -0300

    testes significtivos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('maria joaquina', '33359103020', 2000.0 , TIMESTAMP WITH TIME ZONE '2000-07-14T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('vitor mota', '33359140322', 1000.0 , TIMESTAMP WITH TIME ZONE '2000-04-06T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('joao medeiros', '33375034567', 300.0 , TIMESTAMP WITH TIME ZONE '1999-08-20T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('odilon diniz', '07856556877', 20000.0 , TIMESTAMP WITH TIME ZONE '2000-11-30T11:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('adam smith', '23543478687', 1750.0 , TIMESTAMP WITH TIME ZONE '2010-06-14T12:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('joaquim barbosa', '09764520867', 2150.0 , TIMESTAMP WITH TIME ZONE '2015-10-28T20:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('rosivania brito', '06305257456', 7000.0 , TIMESTAMP WITH TIME ZONE '2020-07-03T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('carmen dias', '47286049539', 315.10 , TIMESTAMP WITH TIME ZONE '1981-06-04T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('flavio dias', '47286049320', 320.10 , TIMESTAMP WITH TIME ZONE '1980-07-14T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('antonia fianca', '59633349320', 3200.40 , TIMESTAMP WITH TIME ZONE '1999-04-20T10:00:00Z', 2);
